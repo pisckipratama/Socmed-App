a3c0c0731a39b306395ab0b5221e9758f67aa8a6
Piscki F. Pratama <pisckipratama@gmail.com>
2020-05-10 06:56:04 +0700

    Home and Profile component done.

--- client/src/components/screens/HomeComponent.jsx
+++ client/src/components/screens/HomeComponent.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 
 const HomeComponent = () => {
   return (
-    <h1>Home</h1>
+    <div className="home">
+      <div className="card home-card">
+        <h5>Stephany Angela</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1542897730-6e8d4bfe5508?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80" alt="monyet" />
+        </div>
+        <div className="card-content">
+          <i className="material-icons">favorite_border</i>
+          <h6>Bali's Women</h6>
+          <p>This is beautiful women from Bali, Indonesia.</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+
+      <div className="card home-card">
+        <h5>Stephany Angela</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1522435229388-6f7a422cd95b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="monyet" />
+        </div>
+        <div className="card-content">
+          <i className="material-icons">favorite_border</i>
+          <h6>Thinker</h6>
+          <p>What the hell are you think?</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+    </div>
   )
 };
 
--- client/src/components/screens/ProfileComponent.jsx
+++ client/src/components/screens/ProfileComponent.jsx
@@ -2,7 +2,29 @@ import React from 'react';
 
 const ProfileComponent = () => {
   return (
-    <h1>Profile</h1>
+    <div className="profile-container">
+      <div className="profile-box">
+        <div>
+          <img className="avatar" src="https://images.unsplash.com/photo-1519625594242-7db544018926?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="avatar" />
+        </div>
+        <div>
+          <h4>Stephany Angela</h4>
+          <div className="profile-detail">
+            <h6>40 posts</h6>
+            <h6>409 followers</h6>
+            <h6>343 following</h6>
+          </div>
+        </div>
+      </div>
+      <div className="gallery">
+        <img src="https://images.unsplash.com/photo-1565651454302-e263192bad3a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" className="item" alt="pictures" />
+        <img src="https://images.unsplash.com/photo-1545230731-051693c8cd7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" className="item" alt="pictures" />
+        <img src="https://images.unsplash.com/photo-1553495280-37259d1e6ebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" className="item" alt="pictures" />
+        <img src="https://images.unsplash.com/photo-1570025796121-87d2d9b69ca9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" className="item" alt="pictures" />
+        <img src="https://images.unsplash.com/flagged/photo-1557527694-b679da68a0c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" className="item" alt="pictures" />
+        <img src="https://images.unsplash.com/photo-1588198280092-6efe98a5dceb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" className="item" alt="pictures" />
+      </div>
+    </div>
   )
 };
 
