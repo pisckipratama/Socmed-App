3c83280b5b90e8052136fbde7430c23639f14a64
Piscki F. Pratama <pisckipratama@gmail.com>
2020-05-10 07:18:51 +0700

    add cors to restful api

--- server/bin/app.js
+++ server/bin/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 const InitiateMongoServer = require('./config/connection');
+const cors = require('cors');
 
 InitiateMongoServer();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const AuthRoutes = require('./routes/auth');
 app.use('/api/users/v1', AuthRoutes);
